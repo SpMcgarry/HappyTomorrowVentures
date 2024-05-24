ebd3b467fcb85e0b94843eaa9ff60790001ee050
Sean McGarry <104318903+SpMcgarry@users.noreply.github.com>
2024-05-24 14:21:43 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AI_Team_Business',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Sean McGarry',
+    author_email='seanmcgarryp@gmail.com',
+    description=''
+)
